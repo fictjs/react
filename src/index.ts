9b5e5babbc6927beb9f272138a467d6685eefb2a
unadlib <unadlib@gmail.com>
2026-02-19 02:43:54 +0800

    feat(types): export react root mount option types

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { reactAction$, reactActionFromQrl } from './action'
 export { ReactIsland, reactify } from './eager'
 export { installReactIslands, type ReactIslandsLoaderOptions } from './loader'
 export { createReactQrl, reactify$ } from './resumable'
+export type { MountedReactRoot, MountReactRootOptions } from './react-root'
 export type {
   ClientDirective,
   MaybeAccessor,
